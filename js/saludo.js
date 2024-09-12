75d5832988d9d94a8708dfe0b66705390d5e24ce
Melissa <melissavare23@gmail.com>
2024-09-12 16:40:31 -0500

    Modificaciones sitio web

--- js/saludo.js
+++ js/saludo.js
@@ -1,3 +1,9 @@
-let nombre=prompt("Digita tu nombre","");
 let contenido=document.getElementById("mensaje");
-contenido.innerHTML="Hola "+nombre;
+if(!localStorage.nom){
+    let nombre=prompt("Digita tu nombre","");
+    localStorage.nom=nombre;
+    contenido.innerHTML="Hola "+nombre;
+}else{
+    contenido.innerHTML=localStorage.nom;
+    
+}
